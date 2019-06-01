09e8758ad5a79584a50f1988d5547e6641886482
elangreen <green.elan5@gmail.com>
2019-05-31 20:36:21 -0700

    added album art to review page for reference

--- src/routes/ReviewPage/ReviewPage.js
+++ src/routes/ReviewPage/ReviewPage.js
@@ -3,7 +3,8 @@ import AlbumApiService from '../../services/albums-api-service'
 
 export class ReviewPage extends Component {
   state = {
-    review: []
+    review: [],
+    album: []
   }
 
   componentDidMount() {
@@ -15,13 +16,21 @@ export class ReviewPage extends Component {
       .catch(err => this.setState({ error: err.error }))
   }
 
+  handleGetAlbum() {
+    AlbumApiService.getAlbum(this.props.match.params.albumId)
+      .then(res => this.setState({ album: res }))
+      .catch(err => this.setState({ error: err.error }))
+  }
+
   render() {
-    const { review } = this.state
+    {this.handleGetAlbum()}
+    const { review, album } = this.state
     return (
       <div className="review-page">
+        <img src={album.art} alt={album.title} />
         <p>{`Title: ${review.title}`}</p>
         <p>{`Rating: ${review.rating}`}</p>
-        <p>{review.title}</p>
+        <p>{review.content}</p>
       </div>
     )
   }
