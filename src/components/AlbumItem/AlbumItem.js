579ec9f92055c9923b8f15209cef4c177712cf43
elangreen <green.elan5@gmail.com>
2019-06-06 20:35:59 -0700

    ratings added

--- src/components/AlbumItem/AlbumItem.js
+++ src/components/AlbumItem/AlbumItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AlbumStarRating from '../../components/AlbumStarRating/AlbumStarRating'
-
 import './AlbumItem.css'
 
 export class AlbumItem extends Component {
