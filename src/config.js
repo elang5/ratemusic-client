d3e259069a70269642ad4e1eab0845e2c1fb8f20
elangreen <green.elan5@gmail.com>
2019-06-10 16:50:46 -0700

    deployment

--- src/config.js
+++ src/config.js
@@ -2,5 +2,3 @@ export default {
   API_ENDPOINT: 'https://shielded-temple-89909.herokuapp.com/api',
   TOKEN_KEY: 'ratemusic-client-auth-token'
 }
-
-// 'https://shielded-temple-89909.herokuapp.com/api'
